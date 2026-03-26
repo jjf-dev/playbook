2b6725ff134031efa8a3d84f280e46124e2db177
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2026-03-26 04:33:13 +0000

    Update published site output

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d39bbda7.js',
+                path_to_root + 'searchindex-5ea9bbab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

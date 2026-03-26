80c86394452b32f8bb8cab4c8f7ac8fbbffc2e15
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2026-03-26 04:42:41 +0000

    Sync site output from asterinas-codex book

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5ea9bbab.js',
+                path_to_root + 'searchindex-930352e1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
